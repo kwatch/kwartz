a23eec0379688db2da0b47b61beb2634c18c2943
makoto kuwata <kwa@kuwata-lab.com>
2005-01-30 22:15:27 +0000

    - [enhance] new file: lib/kwartz/config.rb

---
+++ lib/kwartz/config.rb
@@ -0,0 +1,23 @@
+###
+### copyright(c) 2005 kuwata-lab all rights reserved
+###
+### $Id$
+###
+
+module Kwartz
+   module Config
+      
+      ## translator
+      NEWLINE         = "\n"
+      INDENT          = "  "
+      
+      ## compiler
+      LANG            = "eruby"
+      
+      ## converter
+      ODD_VALUE       = "'odd'"
+      EVEN_VALUE      = "'even'"
+      ATTR_NAME       = "kd"
+      
+   end
+end
