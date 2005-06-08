c6707274756217ae69bd82905d097f4d2a57ccff
makoto kuwata <kwa@kuwata-lab.com>
2005-06-08 23:49:34 +0000

    - [change] example/pagelayout renewed

--- examples/pagelayout/main.rb
+++ examples/pagelayout/main.rb
@@ -1,19 +1,42 @@
 #!/usr/bin/env ruby
 
 ## menu
-menu_list = [
-  { :name => 'Mail',     :url => '/cgi-bin/mail.cgi' },
-  { :name => 'Calnedar', :url => '/cgi-bin/calendar.cgi' },
-  { :name => 'Todo',     :url => '/cgi-bin/todo.cgi' },
+menulist = [
+  { :label => 'Mail',     :url => '/cgi-bin/mail.cgi' },
+  { :label => 'Calnedar', :url => '/cgi-bin/calendar.cgi' },
+  { :label => 'Todo',     :url => '/cgi-bin/todo.cgi' },
+  { :label => 'Stock',    :url => '/cgi-bin/stock.cgi' },
 ]
 
-## title
-title = "My Homepage"
+## contents data
+stocks = [
+  { :symbol => "AAPL", :price => 36.49, :rate => -0.32, },
+  { :symbol => "MSFT", :price => 26.53, :rate => 2.44, },
+  { :symbol => "ORCL", :price => 12.59, :rate => 2.02, },
+  { :symbol => "INTL", :price => 19.51, :rate => 2.90, },
+]
+
+## page filename
+cgi = nil
+symbol = nil
+if ENV['SCRIPT_NAME']
+  require 'cgi'
+  cgi = CGI.new
+  symbol = cgi.params['symbol'].first
+elsif ARGV[0]
+  symbol = ARGV[0]
+end
+if symbol
+  stock = stocks.find { |hash| hash[:symbol] == symbol }
+  filename = 'page2.view'
+else
+  filename = 'page1.view'
+end
 
 ## output
 require 'erb'
-filename = 'page.view'
 str = File.open(filename) { |f| f.read() }
 trim_mode = 1
 erb = ERB.new(str, $SAFE, trim_mode)
+print cgi.header if cgi
 print erb.result(binding())
